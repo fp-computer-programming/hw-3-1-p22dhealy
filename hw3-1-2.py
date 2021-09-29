05b29b34e87e7df3146038ca0b82d958af429e06
p22dhealy <p22dhealy@fairfieldprep.org>
2021-09-29 09:40:15 -0400

    Submit hw 3

--- hw3-1-2.py
+++ hw3-1-2.py
@@ -4,3 +4,9 @@ card1 = input("What is the value of your first card")
 card2 = input("what is the vlaue of your second card")
 
 points = int(card1) + int(card2)
+
+if points > 21:
+    print("You have gone bust!")
+else:
+    print("You are safe!")
+    
---
+++ hw3-1-3.py
@@ -0,0 +1,14 @@
+# author: DMH 9/29/21
+
+number = input("Please enter a number:")
+
+if int(number) % 2 == 0:
+    print("Your number is divisible by 2")
+
+
+if int(number) % 3 == 0:
+    print("Your number is divisible by 3")
+
+
+if int(number) % 5 == 0:
+    print("Your number is divisible by 5")
